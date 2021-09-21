ace5336f761092dceebc5bb098c669bbdd4859f1
WlanKabl <55993320+PhilippStecher@users.noreply.github.com>
2021-09-21 14:22:24 +0200

    :)

--- index.js
+++ index.js
@@ -180,7 +180,6 @@ ReturnHashes = (Obj) => {
 }
 
 Github_Reload = () => {
-    return
     console.log("Github update")
     const a = "../Hidden/Gitpull_RL_Updater.json"
     var b = fs.readFileSync(a, 'utf8');
