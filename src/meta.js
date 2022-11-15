5c13317dbc056dc2151715bb4539875b2bf25feb
WlanKabl <55993320+PhilippStecher@users.noreply.github.com>
2022-11-15 22:41:03 +0100

    iType to iQuality & iCategory

---
+++ src/meta.js
@@ -0,0 +1,28 @@
+exports.quality = [
+    "Common",
+    "Uncommon",
+    "Rare",
+    "Very Rare",
+    "Import",
+    "Exotic",
+    "Painted",
+    "Black Market",
+    "Limited",
+    "Legacy",
+    "Certified"
+]
+
+exports.category = [
+    "Antenna",
+    "Anthem",
+    "Banner",
+    "Body",
+    "Boost",
+    "Decal",
+    "Goal explosion",
+    "Paint finish",
+    "Rocket boost",
+    "Topper",
+    "Trail",
+    "Wheels"
+]
--- src/parse.js
+++ src/parse.js
@@ -2,15 +2,18 @@ const { exec } = require("child_process");
 const cheerio = require("cheerio");
 const hashLib = require("./hash");
 const loggerLib = require('./logger');
+const metaData = require('./meta');
 
 var featuredItemArr = [];
 var dailyItemArr = [];
 
 class ItemConstructor {
-    constructor(iName, iType, iColor, iCertification, iEdition, iPrice, iUpvote, iDownvote) {
+    constructor(iName, iType, iCategory, iQuality, iColor, iCertification, iEdition, iPrice, iUpvote, iDownvote) {
         this.iID = hashLib.itemHash(iName, iType, iColor, iPrice, iCertification, iEdition)
         this.iName = iName;
         this.iType = iType;
+        this.iQuality = iQuality;
+        this.iCategory = iCategory;
         this.iColor = iColor;
         this.iCertification = iCertification;
         this.iEdition = iEdition;
@@ -25,6 +28,33 @@ class ItemConstructor {
     }
 }
 
+var splitType = (iType) => {
+    var iQuality, iCategory = null;
+
+    metaData.category.forEach((filter, index) => {
+        if (iType.toLowerCase().includes(filter.toLowerCase())) {
+            iCategory = filter;
+            return;
+        }
+    })
+
+    metaData.quality.forEach((filter, index) => {
+        if (iType.toLowerCase().includes(filter.toLowerCase())) {
+            iQuality = filter;
+            return;
+        }
+    })
+
+    if (!iQuality || !iCategory) {
+        loggerLib.warn("Quality or category of items couldnt be parsed.\nCheck manuelly!", "parse.js", "0xf0a0ff")
+    }
+
+    return {
+        iCategory: iCategory,
+        iQuality: iQuality
+    }
+}
+
 featuredItemParser = async () => {
     for (var x = 1; x <= 2; x++) {
         var CurrFeat = $(".rlg-item-shop__featured > a:nth-child(" + x + ")").html()
@@ -34,7 +64,12 @@ featuredItemParser = async () => {
         }
         CurrCheer = cheerio.load(CurrFeat.toString());
         var iName = CurrCheer("div.rlg-item-shop__item-content > h1").text().trim();
+
         var iType = CurrCheer("div.rlg-item-shop__item-content > div.rlg-item-shop__item-category").html().replace("<br>", " ");
+        var splitted = splitType(iType);
+        var iQuality = splitted.iQuality;
+        var iCategory = splitted.iCategory;
+
         var iColor = CurrCheer("div.rlg-item-shop__image-meta > div.rlg-item-shop__paint").text().trim();
         /* !..! */
         var iCertification;
@@ -55,7 +90,7 @@ featuredItemParser = async () => {
         var iPrice = CurrCheer("div.rlg-item-shop__meta > div.rlg-item-shop__item-credits").text().trim();
         var iUpvote = CurrCheer("button.rlg-item-shop__item-vote.--up > span").text().trim();
         var iDownvote = CurrCheer("button.rlg-item-shop__item-vote.--down > span").text().trim();
-        featuredItemArr.push(new ItemConstructor(iName, iType, iColor, iCertification, iEdition, iPrice, iUpvote, iDownvote))
+        featuredItemArr.push(new ItemConstructor(iName, iType, iCategory, iQuality, iColor, iCertification, iEdition, iPrice, iUpvote, iDownvote))
     }
     return;
 }
@@ -69,7 +104,12 @@ dailyItemParser = async () => {
         }
         CurrCheer = cheerio.load(CurrDaily.toString());
         var iName = CurrCheer("div.rlg-item-shop__item-content > h1").text().trim();
+
         var iType = CurrCheer("div.rlg-item-shop__item-content > div.rlg-item-shop__item-category").html().replace("<br>", " ");
+        var splitted = splitType(iType);
+        var iQuality = splitted.iQuality;
+        var iCategory = splitted.iCategory;
+
         var iColor = CurrCheer("div.rlg-item-shop__image-meta > div.rlg-item-shop__paint").text().trim();
 
         /* !..! */
@@ -91,7 +131,7 @@ dailyItemParser = async () => {
         var iPrice = CurrCheer("div.rlg-item-shop__meta > div.rlg-item-shop__item-credits").text().trim();
         var iUpvote = CurrCheer("button.rlg-item-shop__item-vote.--up > span").text().trim();
         var iDownvote = CurrCheer("button.rlg-item-shop__item-vote.--down > span").text().trim();
-        dailyItemArr.push(new ItemConstructor(iName, iType, iColor, iCertification, iEdition, iPrice, iUpvote, iDownvote))
+        dailyItemArr.push(new ItemConstructor(iName, iType, iCategory, iQuality, iColor, iCertification, iEdition, iPrice, iUpvote, iDownvote))
     }
     return;
 }
@@ -103,7 +143,7 @@ module.exports.parse = (html, callback) => {
 
     featuredItemParser();
     dailyItemParser();
-
+    
     callback({
         featured: featuredItemArr,
         daily: dailyItemArr
